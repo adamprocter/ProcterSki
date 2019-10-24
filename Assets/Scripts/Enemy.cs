8befcd83894e471e829b901369e04cf99f5042c5
Mathew Parker <mathew.louis.parker@gmail.com>
2019-10-24 15:28:29 +0100

    Add enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿public class Movement : MonoBehaviour
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move please :D
+
+    }
+}
